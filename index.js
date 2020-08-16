2f8114a0138e5d37d1f63f7832bcc94c230a4211
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-08-16 19:27:41 +0700

    feat: add stdout, stderr outputs

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const core = require("@actions/core");
-const parse = require("shell-quote").parse;
+const { parse } = require("shell-quote");
+const { createOutputInterceptor } = require("output-interceptor");
 
 function run() {
+  // Parse inputs.
   const args = core.getInput("args");
   if (typeof args !== "string") {
     throw new Error("args must be a string.");
   }
 
+  // Capture process stdout.
+  createOutputInterceptor({
+    interceptStdout: true,
+    interceptStderr: false,
+  }).then((stdout) => {
+    core.setOutput("stdout", stdout);
+  });
+
+  // Dispatch prettier CLI.
   process.argv = [process.argv[0], process.argv[1], ...parse(args)];
   require("prettier/bin-prettier");
 }
